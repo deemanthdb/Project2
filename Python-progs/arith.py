0fbad377a46b4d69507efd8a3882a9521e3cac15
dheemanth <deemanthdb18@gmail.com>
2023-08-18 14:54:46 +0530

    dision added

--- Python-progs/arith.py
+++ Python-progs/arith.py
@@ -4,8 +4,13 @@ def add(n1,n2):
 def sub(n1,n2):
     return(n1-n2)
 
+def div(n1,n2):
+    return(n1/n2)
+
+
 
 a=int(input("enter num1:"))
 b=int(input("enter num2:"))
 print("sum=",add(a,b))
 print("sum=",sub(a,b))
+print("sum=",div(a,b))
